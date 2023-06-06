4ec4452e334f0a943cd894e1fe34c8aed5553ec5
keviinfoes <kevin.foes@live.nl>
2023-06-06 15:12:16 +0200

    Add remove and adjust

--- app/inheritance-module/src/App.tsx
+++ app/inheritance-module/src/App.tsx
@@ -3,6 +3,7 @@ import { ethers } from 'ethers';
 import styled from 'styled-components'
 import { Button, ButtonLink, Title, GenericModal, Table, TableRow, TextField, AddressInput} from '@gnosis.pm/safe-react-components'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
+import Grid from '@material-ui/core/Grid';
 
 //Flap countdown
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
@@ -20,23 +21,154 @@ const Container = styled.div`
 const SafeApp = (): React.ReactElement => {
   const inheritance_address_goerli = "0xb9cBb0e33C97d9ea46Ae312a508ad2b62fF429AA"
   const { sdk, safe } = useSafeAppsSDK()
+  const moment = require('moment');
 
-
-
-  //Modal for inheritance change
   const [isOpenInheritance, setIsOpenInheritance] = useState(false);
   const [isOpenHeirs, setIsOpenHeirs] = useState(false);
   const [idHeirs, setIdHeirs] = useState("");
   const [hasError, setHasError] = useState<boolean>();
   const [address, setAddress] = useState<string>("0x0000000000000000000000000000000000000000");
-  const [rows, setRows] = useState<TableRow[]>([
-    {
-      id: "0", 
-      cells: [{id: "a", content:"0x0000000000000000000000000000000000000001"}]
-    },
-  ]);
+  const [enabled, setEnabled]  = useState("inactive") 
+  const [deadline, setDeadline]  = useState("0") 
+  const [adjusted, setAdjusted] = useState(false);
+  const [rows, setRows] = useState<TableRow[]>([]);
+
+  // WEB3 CALL FUNCTION - Get inheritance module data
+  useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      //Get module enabled
+      const safe_module_INTERFACE = new ethers.utils.Interface([
+        'function isModuleEnabled(address) external view returns (bool)',
+      ])
+      const encodedSafeModule = safe_module_INTERFACE.encodeFunctionData("isModuleEnabled", [inheritance_address_goerli]);
+      const configModule = {
+        to: safe.safeAddress,
+        data: encodedSafeModule
+      };
+      const responseModule = await sdk.eth.call([configModule])
+      const enabled = ethers.utils.defaultAbiCoder.decode(["bool"], responseModule)
+      setEnabled(enabled[0])
+      //Get inheritance data
+      const Inheritance_INTERFACE = new ethers.utils.Interface([
+        'function heirs(address, uint256) external returns(address)',
+        'function deadline(address) external returns(uint256)',
+      ])
+      const encodedInheritanceDeadline = Inheritance_INTERFACE.encodeFunctionData("deadline", [safe.safeAddress]);
+      const configDeadline = {
+        to: inheritance_address_goerli,
+        data: encodedInheritanceDeadline
+      };
+      const responseDeadline = await sdk.eth.call([configDeadline])
+      const _deadline = ethers.utils.defaultAbiCoder.decode(["uint256"], responseDeadline)
+      setDeadline(_deadline.toString());
+      //Get heirs
+      let end = 1
+      let index = 0
+      const temp = rows
+      if (adjusted === false || temp.length > 0) {
+        while (end === 1) {
+          const encodedInheritanceHeirs = Inheritance_INTERFACE.encodeFunctionData("heirs", [safe.safeAddress, index]);
+          const configHeirs = {
+            to: inheritance_address_goerli,
+            data: encodedInheritanceHeirs
+          };
+          try{
+            const responseHeirs = await sdk.eth.call([configHeirs])
+            const _heir_ = ethers.utils.defaultAbiCoder.decode(["address"], responseHeirs)
+            temp.push({
+              id: responseHeirs, 
+              cells: [{id: responseHeirs, content: _heir_}]
+            })
+            index += 1   
+          } catch (error) {
+            end = 0
+          }  
+        }
+        setRows(temp)
+      }
+      console.log(temp) 
+    }
+    // call the function and catch any error
+    fetchData().catch(console.error);;
+  }, [sdk.eth, safe.safeAddress, rows, adjusted])
+  console.log(enabled)
+
+  // WRITE FUNCTIONS - Add inheritance module to safe
+  const submitTx_addModule = useCallback(async () => {
+    try {   
+      const safe_module_INTERFACE = new ethers.utils.Interface([
+       'function enableModule(address) external',
+      ])
+      const encodedAddModule = safe_module_INTERFACE.encodeFunctionData("enableModule", [inheritance_address_goerli]);
+      const { safeTxHash } = await sdk.txs.send({
+        txs: [
+          {
+            to: safe.safeAddress,
+            value: '0',
+            data: encodedAddModule,
+          },
+        ],
+      })
+      console.log({ safeTxHash })
+      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
+      console.log({ safeTx })
+    } catch (e) {
+      console.error(e)
+    }
+  }, [safe, sdk])
+  //Store inheritance
+  const submitTx_addInheritance = useCallback(async (heirs, _deadline) => {
+    try {   
+      const inheritance_module_INTERFACE = new ethers.utils.Interface([
+       'function setInheritance(address[], uint256) external',
+      ])
+      const encodedAddInheritance = inheritance_module_INTERFACE.encodeFunctionData("setInheritance", [heirs, _deadline]);
+      const { safeTxHash } = await sdk.txs.send({
+        txs: [
+          {
+            to: inheritance_address_goerli,
+            value: '0',
+            data: encodedAddInheritance,
+          },
+        ],
+      })
+      console.log({ safeTxHash })
+      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
+      console.log({ safeTx })
+    } catch (e) {
+      console.error(e)
+    }
+  }, [sdk])
+  //Remove inheritance
+  const submitTx_removeInheritance = useCallback(async () => {
+    try {   
+      const inheritance_module_INTERFACE = new ethers.utils.Interface([
+       'function removeInheritance() external',
+      ])
+      const encodeRemoveInheritance = inheritance_module_INTERFACE.encodeFunctionData("removeInheritance");
+      const { safeTxHash } = await sdk.txs.send({
+        txs: [
+          {
+            to: inheritance_address_goerli,
+            value: '0',
+            data: encodeRemoveInheritance,
+          },
+        ],
+      })
+      console.log({ safeTxHash })
+      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
+      console.log({ safeTx })
+    } catch (e) {
+      console.error(e)
+    }
+  }, [sdk])
+
+
+  //Modal for inheritance change
   const handleDate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value)
+    const timestamp = new Date(event.currentTarget.value).getTime() / 1000
+    setDeadline(timestamp.toString())
   }
   useEffect(() => {
     setHasError(!ethers.utils.isAddress(address));
@@ -54,6 +186,7 @@ const SafeApp = (): React.ReactElement => {
   const removeAddressHeirs = () => {
     let temp = rows
     temp = temp.filter((obj) => obj.id !== idHeirs)
+    setAdjusted(true)
     setRows(temp)
     setIsOpenHeirs(false)
   }
@@ -79,22 +212,20 @@ const SafeApp = (): React.ReactElement => {
         Store
       </Button>
     </div>
-
-    
   const addAddressHeirs = () => {
-    
-    setIsOpenHeirs(!isOpenHeirs)
-    
-
+    const dateTime = Date.now();
+    const timestamp = Math.floor(dateTime / 1000);
     const temp = rows
+    const _id = timestamp.toString()
     temp.push({
-      id: "1", 
-      cells: [{id: "b", content:"0x0000000000000000000000000000000000000002"}]
+      id: _id, 
+      cells: [{id: _id, content:"0x0000000000000000000000000000000000000000"}]
     })
     setRows(temp)
+    setAddress("0x0000000000000000000000000000000000000000")
+    setIdHeirs(_id)
+    setIsOpenHeirs(!isOpenHeirs)
   }
-
-
   const adjustAddressHeirs = (event: string) => {
     setIsOpenHeirs(!isOpenHeirs)
     const objWithIdIndex = rows.findIndex((obj) => obj.id === event);  
@@ -105,29 +236,47 @@ const SafeApp = (): React.ReactElement => {
     }
     setIsOpenHeirs(!isOpenHeirs)
   }
+  const submitInheritance = () => {
+    const tempHeirs:string[] = []
+    rows.forEach((row) => {
+      const _heir = row.cells[0].content?.toString()
+      if(_heir) {
+        tempHeirs.push(_heir)
+      }
+    })
+    const sort = tempHeirs.sort()
+    submitTx_addInheritance(sort, deadline)
+  }
+  const start_time = new Date(parseInt(deadline) * 1000)
+  const _date = moment(start_time).format("YYYY-MM-DDTHH:mm")
   const modal_body_inheritance =
     <div style={{textAlign:"center"}}>
-      <ButtonLink color="primary" iconType="add" onClick={addAddressHeirs}style={{marginBottom: '1rem'}}>
-        Add heir
-      </ButtonLink>
+      <Grid container justifyContent="space-around">
+        <ButtonLink color="primary" iconType="add" onClick={addAddressHeirs} style={{marginBottom: '1rem'}}>
+          Add heir
+        </ButtonLink>
+        <ButtonLink color="error" iconType="filledCross" onClick={submitTx_removeInheritance} style={{marginLeft: '20rem', marginBottom: '1rem'}}>
+          Remove Inheritance
+        </ButtonLink>
+      </Grid >
       <Table 
         rows={rows}
         onRowClick={adjustAddressHeirs}
       />
      <form className="" noValidate>
       <TextField
-        id="date"
+        id="datetime-local"
         label="Deadline"
-        type="date"
-        defaultValue="2023-05-24"
+        type="datetime-local"
         className=""
+        defaultValue={_date}
         InputLabelProps={{
           shrink: true,
         }}
         onChange={handleDate}
       />
     </form>
-      <Button size="lg" color="primary" style={{marginTop: '1rem'}}>
+      <Button size="lg" color="primary" onClick={submitInheritance} style={{marginTop: '1rem'}}>
         Submit change
       </Button>
       {isOpenHeirs && (
@@ -139,89 +288,15 @@ const SafeApp = (): React.ReactElement => {
         />
       )}
     </div>
-    
-        
-
-
-
-
-  // Get inheritance module data
-  const [enabled, setEnabled]  = useState("inactive") 
-  const [deadline, setDeadline]  = useState("inactive") 
-  useEffect(() => {
-    // declare the async data fetching function
-    const fetchData = async () => {
-      //Get module enabled
-      const safe_module_INTERFACE = new ethers.utils.Interface([
-        'function isModuleEnabled(address) external view returns (bool)',
-      ])
-      const encodedSafeModule = safe_module_INTERFACE.encodeFunctionData("isModuleEnabled", [inheritance_address_goerli]);
-      const configModule = {
-        to: safe.safeAddress,
-        data: encodedSafeModule
-      };
-      const responseModule = await sdk.eth.call([configModule])
-      const enabled = ethers.utils.defaultAbiCoder.decode(["bool"], responseModule)
-      setEnabled(enabled[0])
-
-      //Get inheritance data
-      const Inheritance_INTERFACE = new ethers.utils.Interface([
-        'function heirs(address) external returns(address[])',
-        'function deadline(address) external returns(uint256)',
-      ])
-      const encodedInheritanceDeadline = Inheritance_INTERFACE.encodeFunctionData("deadline", [safe.safeAddress]);
-      const configDeadline = {
-        to: '0xb9cBb0e33C97d9ea46Ae312a508ad2b62fF429AA',
-        data: encodedInheritanceDeadline
-      };
-      const responseDeadline = await sdk.eth.call([configDeadline])
-      const _deadline = ethers.utils.defaultAbiCoder.decode(["uint256"], responseDeadline)
-      setDeadline(_deadline.toString());
-    }
-    // call the function and catch any error
-    fetchData().catch(console.error);;
-  }, [sdk.eth, safe.safeAddress])
-  console.log(enabled)
-  console.log(deadline)
-
-
-  //Add inheritance module to safe
-  const submitTx_addModule = useCallback(async () => {
-    try {   
-      const safe_module_INTERFACE = new ethers.utils.Interface([
-       'function enableModule(address) external',
-      ])
-      const encodedAddModule = safe_module_INTERFACE.encodeFunctionData("enableModule", [inheritance_address_goerli]);
-      const { safeTxHash } = await sdk.txs.send({
-        txs: [
-          {
-            to: safe.safeAddress,
-            value: '0',
-            data: encodedAddModule,
-          },
-        ],
-      })
-      console.log({ safeTxHash })
-      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
-      console.log({ safeTx })
-    } catch (e) {
-      console.error(e)
-    }
-  }, [safe, sdk])
-
 
   return (
     <Container>
       <Title size="md" withoutMargin={false}>Safe: {safe.safeAddress}</Title>
-
-
       <Button size="lg" color="primary" onClick={() => submitTx_addModule()} style={{marginBottom: '3rem'}}>
         Add inheritance module
       </Button>
-
-
       <FlipClockCountdown 
-        to={new Date().getTime() + 31556926 * 2 * 1000}
+        to={parseInt(deadline) * 1000}
         labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
         labelStyle={{ color: 'black', fontSize: 14, fontWeight: 500, textTransform: 'uppercase' }}
         digitBlockStyle={{ width: 40, height: 60, fontSize: 30 }}
@@ -231,8 +306,6 @@ const SafeApp = (): React.ReactElement => {
       >
         FINISHED
       </FlipClockCountdown>
-
-
       <Button size="lg" color="primary" onClick={() => setIsOpenInheritance(!isOpenInheritance)} style={{marginTop: '3rem'}}>
         Change inheritance
       </Button>
@@ -245,6 +318,9 @@ const SafeApp = (): React.ReactElement => {
         />
       )}
 
+      <Button size="lg" color="secondary" style={{marginTop: '3rem'}}>
+        Carry out inheritance
+      </Button>
     </Container>
   )
 }
@@ -258,10 +334,12 @@ export default SafeApp
 x   1. DEPLOY INHERITANCE ON GOERLI => 0xb9cBb0e33C97d9ea46Ae312a508ad2b62fF429AA
 x   1a. call contract from app 
 x   1b. add call add inheritance
-    1c. add call add/change inheritance
-    1d. add call remove inheritance
-    1e. add call execute inheritance
-    1x. check calls above for multiple owner safe
+x   1c. add call add/change inheritance
+x   1d. get data current heirs to display - when inheritance active
+x   1e. add call remove inheritance
+
+    1-. add call execute inheritance
+    1-. check calls above for multiple owner safe
 
 --
    2. CHECK SAFE WALLET = MODEL 
